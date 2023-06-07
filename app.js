4e6582fda882d15074d4c4827b0842bbefebf2fe
Guilherme Souza <delayggz@hotmail.com>
2023-06-07 09:06:33 -0300

    Criacao de carroussel para cards dos produtos, mais adicao de novos produtos

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $(".container-banners").slick({
   nextArrow:'.arrow-next',
-  prevArrow:'.arrow-prev'
+  prevArrow:'.arrow-prev',
+  autoplay: true,
+  speed:1000
 })
-
+$('.card-products').slick({
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  prevArrow:'.card-prev',
+  nextArrow:'.card-next'
+});
