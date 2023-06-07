251f0b2d9664b7393fd78de44f6be1b4669bcac5
Guilherme Souza <delayggz@hotmail.com>
2023-06-07 09:18:04 -0300

    Ajuste na arrow do banner, e organizacao do script dos carrousel

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-$(".container-banners").slick({
-  nextArrow:'.arrow-next',
-  prevArrow:'.arrow-prev',
-  autoplay: true,
-  speed:1000
-})
-$('.card-products').slick({
-  speed: 300,
-  slidesToShow: 6,
-  slidesToScroll: 2,
-  prevArrow:'.card-prev',
-  nextArrow:'.card-next'
-});
+function ativarCarrousel(){
+  $(".container-banners").slick({
+    nextArrow:'.arrow-next',
+    prevArrow:'.arrow-prev',
+    autoplay: true,
+    speed:1000
+  })
+  $('.card-products').slick({
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    prevArrow:'.card-prev',
+    nextArrow:'.card-next'
+  });
+}
+ativarCarrousel();
