72793633490c89ca61649b1f1c2579faeaf02ca4
Gabriel <damascenosd@hotmail.com>
2023-06-10 17:37:14 -0300

    index js2

--- js/modules/add_to_cart.js
+++ js/modules/add_to_cart.js
@@ -7,7 +7,6 @@ export default function initAddtoCart(){
   addCartBtns.forEach((btn)=>{
     btn.addEventListener("click",enviarDataLayer);
     btn.addEventListener("click",ativarCart);
-    window.dataLayer.push(dataLayerAddToCart);
   });
 
   cartCloseBtn.addEventListener("click",desativarCart);
@@ -24,6 +23,7 @@ function enviarDataLayer(){
       "item_name":nomeProduto,
     }]
 };
+  window.dataLayer.push(dataLayerAddToCart);
   console.log(dataLayerAddToCart); 
 }
 
