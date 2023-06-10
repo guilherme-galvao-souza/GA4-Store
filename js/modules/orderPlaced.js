080e74b1f2583a44f3e76f7146d499ee566d8145
Gabriel <damascenosd@hotmail.com>
2023-06-10 19:47:53 -0300

    confirmation

---
+++ js/modules/orderPlaced.js
@@ -0,0 +1,16 @@
+window.dataLayer.push ({
+    'event':'confirmation',
+    'currency': "BRL",
+    'transaction_id': "1234",
+    "value": "1.180",
+      add: {
+        products: [
+          {
+            name: "Celular 128GB/6GB RAM",
+            id: "1234",
+            price: "1.180",
+          }
+        ]
+    },
+
+})
